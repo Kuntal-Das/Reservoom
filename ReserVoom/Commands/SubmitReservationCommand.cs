f1bf45472178325a3a87c3afb72db044eb79b471
Kuntal Das <25ckuntaldas@gmail.com>
2022-05-30 22:29:26 +0530

    Commands

---
+++ ReserVoom/Commands/SubmitReservationCommand.cs
@@ -0,0 +1,70 @@
+﻿using Exceptions;
+using Models;
+using ReserVoom.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ReserVoom.Commands
+{
+    public class SubmitReservationCommand : CommandBase
+    {
+        private readonly MakeReservationViewModel _makeReservationViewModel;
+        private readonly Hotel _hotel;
+
+        public SubmitReservationCommand(MakeReservationViewModel makeReservationViewModel, Hotel hotel)
+        {
+            _makeReservationViewModel = makeReservationViewModel;
+            _hotel = hotel;
+
+            _makeReservationViewModel.PropertyChanged += OnViewModelPropertychanged;
+        }
+
+        private void OnViewModelPropertychanged(object? sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == nameof(MakeReservationViewModel.UserName)
+                || e.PropertyName == nameof(MakeReservationViewModel.FloorNumber)
+                || e.PropertyName == nameof(MakeReservationViewModel.RoomNumber))
+            {
+                OnCanExecuteChanged();
+            }
+        }
+
+        public override bool CanExecute(object? parameter)
+        {
+            return
+                !string.IsNullOrWhiteSpace(_makeReservationViewModel.UserName)
+                && _makeReservationViewModel.FloorNumber > 0
+                && _makeReservationViewModel.RoomNumber > 0
+                && base.CanExecute(parameter);
+        }
+
+        public override void Execute(object? parameter)
+        {
+            Reservation reservation = new Reservation(
+                _makeReservationViewModel.UserName,
+                new RoomID(_makeReservationViewModel.FloorNumber, _makeReservationViewModel.RoomNumber),
+                _makeReservationViewModel.StartDate,
+                _makeReservationViewModel.EndDate
+                );
+
+            try
+            {
+                _hotel.MakeReservation(reservation);
+
+                MessageBox.Show("Successfully booked.", "Success",
+                 MessageBoxButton.OK, MessageBoxImage.Information);
+
+            }
+            catch (ReservationConflictException)
+            {
+                MessageBox.Show("This room is already taken.", "Error",
+                    MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+    }
+}
